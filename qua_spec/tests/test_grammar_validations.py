f0afd22997ba00a5be7be588d93ff5e4bdda8ccd
Tal <tshani@gmail.com>
2021-07-05 12:45:52 +0000

    Adding test to validate grammer files

---
+++ qua_spec/tests/test_grammar_validations.py
@@ -0,0 +1,14 @@
+import pytest
+from qua_spec.grammar import load_grammar
+import os
+
+@pytest.mark.parametrize("grammar", [1,2])
+def test_all_validations_have_valid_rule(grammar):
+    from jsonschema import validate
+    import yaml
+    import json
+    with open(os.path.abspath(__file__ + "/../../ast_definition.schema.json"), "r+") as f:
+        schema = json.loads(f.read())
+    with open(os.path.abspath(f"{__file__ }/../../qua{grammar}_grammar.yaml"), "r+") as f:
+        grammar = yaml.safe_load(f.read())
+    validate(instance=grammar, schema=schema)
